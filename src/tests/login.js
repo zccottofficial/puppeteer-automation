c395b13a04c710b1d0f8be49ae01695d7cf6db75
rahulap-aot <rahul.ap@aot-technologies.com>
2024-11-07 23:38:22 -0800

    quipo

--- src/tests/login.js
+++ src/tests/login.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 
 async function launchBrowser(extensionPath) {
   return await puppeteer.launch({
-    headless: false,
+    headless: true,
     timeout: 120000,
     args: [
       '--no-sandbox',
